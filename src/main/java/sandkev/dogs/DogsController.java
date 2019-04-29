3b7ed8acffa86d0ce73a536e5b4e8680740cbd34
Kevin Sanders <kevsanders@gmail.com>
2019-04-29 22:49:55 +0100

    yaml examples

--- src/main/java/sandkev/dogs/DogsController.java
+++ src/main/java/sandkev/dogs/DogsController.java
@@ -2,6 +2,7 @@ package sandkev.dogs;
 
 import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
 
 import static spark.Spark.get;
@@ -10,9 +11,11 @@ import static spark.Spark.post;
 import static spark.Spark.delete;
 
 import java.util.List;
+import java.util.Optional;
 
 @Value
 @AllArgsConstructor(staticName = "of")
+@Builder(buildMethodName = "buildInternal")
 public class DogsController {
     MockDogProvider service;
 
@@ -30,7 +33,18 @@ public class DogsController {
 
     }
 
+    /** Add some functionality to the generated builder class */
+    public static class DogsControllerBuilder {
+        public Optional<DogsController> buildOptional() {
+            return Optional.of(this.build());
+        }
 
+        public DogsController build() {
+            DogsController controller = this.buildInternal();
+            controller.register();
+            return controller;
+        }
+    }
 }
 
 /*
